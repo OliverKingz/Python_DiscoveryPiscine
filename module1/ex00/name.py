6e4d42fa2bf4f1f91a070c7b4f78ea1976a881eb
OliverKingZ <oliverkingzamora@gmail.com>
2025-05-12 15:56:50 +0200

    module1 done

---
+++ module1/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Oliver"
+last_name = "Zamora"
+print(first_name + " " + last_name)
---
+++ module1/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Oliver"
+last_name = "Zamora"
+whole_name = first_name + " " + last_name
+print(whole_name)
