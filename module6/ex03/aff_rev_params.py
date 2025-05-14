8eb044c4aed13ffcd5274a6ddd890ccf0551df84
OliverKingZ <oliverkingzamora@gmail.com>
2025-05-14 15:02:48 +0200

    module6 done

--- module6/ex03/aff_rev_params.py
+++ module6/ex03/aff_rev_params.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
 
+import sys
+
+# if len(sys.argv) > 2:
+# 	for i in range(1, len(sys.argv)):
+# 		print(sys.argv[len(sys.argv) - i])
+# else:
+# 	print("none")
+
+## Another method
+if len(sys.argv) > 2:
+	reversed_args = sys.argv[1:][::-1]
+	for arg in reversed_args:
+		print(arg)
+else:
+ 	print("none")
--- module6/ex04/scan_it.py
+++ module6/ex04/scan_it.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3
 
+import sys, re
+
+if len(sys.argv) == 3:
+	keyword = sys.argv[1]
+	to_be_searched = sys.argv[2]
+	count_keywords = re.findall(keyword, to_be_searched).count(keyword)
+	print(count_keywords)
+else:
+	print("none")
